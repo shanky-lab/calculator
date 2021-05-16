819ab7cf7a9b76637354d9e1042b1d1d0b9ab4a0
shanky-lab <savantsb.savant@gmail.com>
2021-05-16 21:30:02 +0530

    First Commit

---
+++ calculator/src/main/java/com/sil/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package com.sil.calculator;
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return (a + b);
+    }
+
+    public static int sub(int a, int b) {
+        return (a - b);
+    }
+
+    public static int mult(int a, int b) {
+        return (a * b);
+    }
+
+    public static int dev(int a, int b) {
+        return (a / b);
+    }
+}
